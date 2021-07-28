4d45f3df2a6cbf0e22540d6406cc238ebe5b3633
King Lemming <kinglemming@gmail.com>
2021-07-28 14:02:59 -0700

    1.3.0 ready? :)
    Signed-off-by: King Lemming <kinglemming@gmail.com>

--- ThermalExpansion/src/main/java/cofh/thermal/expansion/compat/patchouli/CatalystProcessor.java
+++ ThermalExpansion/src/main/java/cofh/thermal/expansion/compat/patchouli/CatalystProcessor.java
@@ -3,7 +3,6 @@ package cofh.thermal.expansion.compat.patchouli;
 import cofh.thermal.core.util.managers.machine.InsolatorRecipeManager;
 import cofh.thermal.core.util.managers.machine.PulverizerRecipeManager;
 import cofh.thermal.core.util.managers.machine.SmelterRecipeManager;
-import cofh.thermal.lib.util.managers.AbstractManager;
 import cofh.thermal.lib.util.managers.CatalyzedRecipeManager;
 import net.minecraft.item.ItemStack;
 import vazkii.patchouli.api.IComponentProcessor;
@@ -13,11 +12,15 @@ import vazkii.patchouli.api.IVariableProvider;
 import java.util.ArrayList;
 import java.util.List;
 
+import static cofh.lib.util.helpers.StringHelper.localize;
+
 public class CatalystProcessor implements IComponentProcessor {
+
     private ArrayList<CatalyzedRecipeManager> managers = new ArrayList<>();
 
     @Override
     public void setup(IVariableProvider variables) {
+
         managers.add(SmelterRecipeManager.instance());
         managers.add(PulverizerRecipeManager.instance());
         managers.add(InsolatorRecipeManager.instance());
@@ -25,6 +28,7 @@ public class CatalystProcessor implements IComponentProcessor {
 
     @Override
     public IVariable process(String key) {
+
         if (managers.size() == 0)
             return null;
         String[] keys = key.split("_");
@@ -32,28 +36,27 @@ public class CatalystProcessor implements IComponentProcessor {
 
         if (machineIndex < managers.size()) {
             if (keys.length == 2) {
-                switch(keys[1]) {
+                switch (keys[1]) {
                     case "machine":
                         if (managers.get(machineIndex) instanceof SmelterRecipeManager)
-                            return IVariable.wrap("Smelter");
+                            return IVariable.wrap(localize("block.thermal.machine_smelter"));
                         if (managers.get(machineIndex) instanceof PulverizerRecipeManager)
-                            return IVariable.wrap("Pulverizer");
+                            return IVariable.wrap(localize("block.thermal.machine_pulverizer"));
                         if (managers.get(machineIndex) instanceof InsolatorRecipeManager)
-                            return IVariable.wrap("Insolator");
+                            return IVariable.wrap(localize("block.thermal.machine_insolator"));
                     case "enablejei":
                         return IVariable.wrap(managers.get(machineIndex).getCatalysts().size() > 6);
                     default:
                         return null;
                 }
-            }
-            else {
+            } else {
                 CatalyzedRecipeManager manager = managers.get(machineIndex);
                 List<ItemStack> catalysts = manager.getCatalysts();
                 int catalystIndex = Integer.parseInt(keys[1]);
 
                 if (catalystIndex < catalysts.size() && (catalystIndex < 5 || (catalystIndex == 5 && catalysts.size() < 7))) {
                     ItemStack catalyst = catalysts.get(catalystIndex);
-                    switch(keys[2]) {
+                    switch (keys[2]) {
                         case "enable":
                             return IVariable.wrap(true);
                         case "item":
@@ -74,4 +77,5 @@ public class CatalystProcessor implements IComponentProcessor {
         }
         return null;
     }
+
 }
